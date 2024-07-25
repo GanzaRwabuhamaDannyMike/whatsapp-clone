04d574fed494c5e15b7a948eee234c6917d17cc0
GanzaRwabuhamaDannyMike <157746677+GanzaRwabuhamaDannyMike@users.noreply.github.com>
2024-07-25 14:41:13 +0200

    2-Webhooks added

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://possible-louse-72.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ReactNode } from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+        {children}
+        </SignedIn>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
