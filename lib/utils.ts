28d797f98805f6f745a610ab0be1b2f2a682d190
GanzaRwabuhamaDannyMike <157746677+GanzaRwabuhamaDannyMike@users.noreply.github.com>
2024-07-26 16:22:21 +0200

    6- video-call feature added

--- lib/utils.ts
+++ lib/utils.ts
@@ -89,11 +89,11 @@ export function randomID(len: number) {
 	let result = "";
 	if (result) return result;
 	var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
-		maxPos = chars.length,
-		i;
+	  maxPos = chars.length,
+	  i;
 	len = len || 5;
 	for (i = 0; i < len; i++) {
-		result += chars.charAt(Math.floor(Math.random() * maxPos));
+	  result += chars.charAt(Math.floor(Math.random() * maxPos));
 	}
 	return result;
-}
+  }
