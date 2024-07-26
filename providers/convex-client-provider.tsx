7fc7c640a634ef1ea61320294f7f48c763b3bf33
GanzaRwabuhamaDannyMike <157746677+GanzaRwabuhamaDannyMike@users.noreply.github.com>
2024-07-26 17:47:07 +0200

    6-Landing page styled

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  useAuth,
+} from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
@@ -14,11 +22,27 @@ export function ConvexClientProvider({ children }: { children: ReactNode }) {
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <SignedOut>
-            <SignInButton />
+          <main className='m-5'>
+            <div className='flex flex-col items-center justify-center mx-auto h-[calc(100vh-50px)] max-w-[1700px] bg-left-panel border-4'>
+              <div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
+              <Image
+                src={"/desktop-hero.png"}
+                alt='Hero'
+                width={320}
+                height={188}
+              />
+              <p className='text-3xl font-extralight mt-5 mb-2'>
+                WhatsApp Clone
+              </p>
+              <SignInButton>
+                <Button className='rounded-full my-5 bg-green-primary hover:bg-green-secondary'>
+                  Sign In to continue
+                </Button>
+              </SignInButton>
+            </div>
+          </main>
         </SignedOut>
-        <SignedIn>
-        {children}
-        </SignedIn>
+        <SignedIn>{children}</SignedIn>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
